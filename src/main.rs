6d05f8c8b995947b419bbf33656beb44183256fa
Soth-dev <mcbedrockplayer2008@gmail.com>
2026-02-10 11:40:38 +0700

    run exec file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     io::{self, Write},
     os::unix::fs::PermissionsExt,
     path::Path,
+    process::{Command, Stdio},
 };
 
 fn main() {
@@ -18,37 +19,51 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         let _ = io::stdin().read_line(&mut input);
-        let (cmd, arg) = match input.trim().split_once(' ') {
+        let (cmd, args) = match input.trim().split_once(' ') {
             Some((fst, snd)) => (fst, snd),
-            None => (input.trim(), ""),
+            _ => (input.trim(), ""),
         };
         match cmd {
+            "" => continue,
             "exit" => break,
-            "echo" => println!("{}", arg),
-            "type" => match arg {
-                "echo" | "exit" | "type" => println!("{} is a shell builtin", arg),
-                _ => {
-                    let mut done = false;
-                    for path in &paths {
-                        if Path::new(path).join(arg).is_file() {
-                            match metadata(format!("{}/{}", path, arg)) {
-                                Ok(m) => {
-                                    if m.permissions().mode() & 0o111 != 0 {
-                                        println!("{} is {}/{}", arg, path, arg);
-                                        done = true;
-                                        break;
-                                    }
-                                }
-                                Err(_) => continue,
-                            }
-                        }
-                    }
-                    if !done {
-                        println!("{}: not found", arg);
-                    }
-                }
+            "echo" => println!("{}", args),
+            "type" => match args {
+                "echo" | "exit" | "type" => println!("{} is a shell builtin", args),
+                _ => match check_exec(&paths, args) {
+                    Some(path) => println!("{} is {}", args, path),
+                    _ => println!("{}: not found", args),
+                },
             },
-            any => println!("{}: command not found", any),
+            _ => match check_exec(&paths, cmd) {
+                Some(_path) => {
+                    let _run = Command::new(cmd)
+                        .args(args.split_whitespace())
+                        //.env("PATH", path)
+                        .stdout(Stdio::inherit())
+                        .stderr(Stdio::inherit())
+                        .spawn()
+                        .expect("err")
+                        .wait()
+                        .expect("err");
+                }
+                _ => println!("{}: command not found", cmd),
+            },
         }
     }
+}
+
+fn check_exec(paths: &Vec<String>, file: &str) -> Option<String> {
+    for path in paths {
+        if Path::new(path).join(file).is_file() {
+            match metadata(format!("{}/{}", path, file)) {
+                Ok(m) => {
+                    if m.permissions().mode() & 0o111 != 0 {
+                        return Some(format!("{}/{}", path, file));
+                    }
+                }
+                Err(_) => continue,
+            }
+        }
+    }
+    None
 }
